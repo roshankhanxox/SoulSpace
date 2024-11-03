8b4a24cde968952e9f6f1e4cb44e72fa755958a9
Roshan Khan <useranroid0@gmail.com>
2024-11-03 18:49:37 +0530

    longashelld

---
+++ soulspace/urls.py
@@ -0,0 +1,8 @@
+# soulspace/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/", include("api.urls")),  # Include the API app URLs
+]
